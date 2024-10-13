47b28acefbb6dc1ef1b616789f1bd1eb03390ad4
Juan Sebastian Valencia Londoño <valencialondonojuansebastian@gmail.com>
2024-10-12 23:11:19 -0500

    Preparando despliegue

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2u&hj9ghg5rn!sb3v21t)g5mlzt1l)i%#z44fna#)2g)h@f5&o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False # despues de producción
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -154,6 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
